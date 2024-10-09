6f11e1ec73a23f3ca19c72fb3615db80ee7ba7c4
rakib13x <rakib13x@gmail.com>
2024-10-10 05:42:05 +0600

    trying again to fix some erros

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ['http:localhost:5173', 'https://assignmen-5-client.vercel.app'],
+    origin: ['https://assignmen-5-client.vercel.app'],
     credentials: true,
   }),
 );
