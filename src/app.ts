af3e2cd91d4e7def2ce51e892f25de836b0cdc59
rakib13x <rakib13x@gmail.com>
2024-10-10 06:35:17 +0600

    trying to fix some error

--- src/app.ts
+++ src/app.ts
@@ -15,47 +15,47 @@ const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(
+  cors({
+    origin: ['https://assignmen-5-client.vercel.app'],
+    credentials: true,
+  }),
+);
+
+// const allowedOrigins = [
+//   'http://localhost:5173',
+//   'https://assignmen-5-client.vercel.app',
+// ];
+
 // app.use(
 //   cors({
-//     origin: ['https://assignmen-5-client.vercel.app'],
+//     origin: (origin: string | undefined, callback) => {
+//       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//         // Allow request if origin is allowed or origin is undefined (non-browser requests)
+//         callback(null, true);
+//       } else {
+//         // Reject request if origin is not allowed
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     },
 //     credentials: true,
 //   }),
 // );
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://assignmen-5-client.vercel.app',
-];
-
-app.use(
-  cors({
-    origin: (origin: string | undefined, callback) => {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        // Allow request if origin is allowed or origin is undefined (non-browser requests)
-        callback(null, true);
-      } else {
-        // Reject request if origin is not allowed
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-  }),
-);
-
-// Handle preflight requests (OPTIONS)
-app.options(
-  '*',
-  cors({
-    origin: (origin: string | undefined, callback) => {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-  }),
-);
+// // Handle preflight requests (OPTIONS)
+// app.options(
+//   '*',
+//   cors({
+//     origin: (origin: string | undefined, callback) => {
+//       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     },
+//     credentials: true,
+//   }),
+// );
 
 // app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
